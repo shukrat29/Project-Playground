c5e8b6ee0ce2b1211afa3117fe42f804d7cea0d5
Shukra Tamang <shukratamang.npl@gmail.com>
2025-01-22 13:28:00 +1100

    header and footer

---
+++ store/lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Clothing Store";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "A modern ecommerce app built with nex js";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
